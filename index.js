0d6f74645496ea876f2f7bbf7ccc2604155daa90
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-06-20 16:13:31 +0300

    Refactor routes and middleware in index.js

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 
-let server = require('./qr'),
-    code = require('./pair');
+let server = require('./qr');
+let code = require('./pair');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
+// Middleware first
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
 app.use('/server', server);
 app.use('/code', code);
 
-app.use('/pair', async (req, res) => {
-    res.sendFile(__path + '/pair.html');
+app.get('/pair', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'pair.html'));
 });
 
-app.use('/qr', async (req, res) => {
-    res.sendFile(__path + '/qr.html');
+app.get('/qr', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'qr.html'));
 });
 
-app.use('/', async (req, res) => {
-    res.sendFile(__path + '/main.html');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'main.html'));
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Error handling
+app.use((req, res) => {
+    res.status(404).send('Page not found');
+});
 
 app.listen(PORT, () => {
     console.log(`
